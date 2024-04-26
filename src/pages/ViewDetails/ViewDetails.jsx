e87b82babc531b898aacbc7436461091cca321d1
rznayeem <riyazulnayeem@gmail.com>
2024-04-26 22:52:56 +0600

    created view details section

--- src/pages/ViewDetails/ViewDetails.jsx
+++ src/pages/ViewDetails/ViewDetails.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ViewDetails = () => {
-  return <div></div>;
+  const spotData = useLoaderData();
+
+  const {
+    _id,
+    photo,
+    email,
+    name,
+    spot_name,
+    country_name,
+    location,
+    description,
+    cost,
+    seasonality,
+    travel_time,
+    visitors,
+  } = spotData;
+
+  return (
+    <div className="container mx-auto">
+      <img
+        className="rounded-3xl max-h-[60vh] w-full object-cover"
+        src={photo}
+        alt=""
+      />
+      <div className="flex gap-12">
+        <div className="w-[65%]">
+          <h1>Details</h1>
+          <p>{description}</p>
+        </div>
+        <div>
+          <p>Price:{cost}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ViewDetails;
